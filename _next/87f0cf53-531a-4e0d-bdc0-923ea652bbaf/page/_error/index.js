3d7ba0a8cf11e634927595068a8e7f132a886beb
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2017-05-17 21:16:09 +0200

    Updates

--- _next/87f0cf53-531a-4e0d-bdc0-923ea652bbaf/page/_error/index.js
+++ _next/87f0cf53-531a-4e0d-bdc0-923ea652bbaf/page/_error/index.js
@@ -1,6 +1,6 @@
 
           window.__NEXT_REGISTER_PAGE('/_error', function() {
-            var comp = module.exports=webpackJsonp([4],[],[331]);
+            var comp = module.exports=webpackJsonp([4],[],[332]);
             return { page: comp.default }
           })
         
