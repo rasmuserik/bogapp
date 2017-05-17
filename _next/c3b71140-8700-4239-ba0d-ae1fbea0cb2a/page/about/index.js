5e6a7f066b0fa5f1a10f248ed4500805a80b3454
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2017-05-17 21:55:41 +0200

    Updates

--- _next/c3b71140-8700-4239-ba0d-ae1fbea0cb2a/page/about/index.js
+++ _next/c3b71140-8700-4239-ba0d-ae1fbea0cb2a/page/about/index.js
@@ -1,6 +1,6 @@
 
           window.__NEXT_REGISTER_PAGE('/about', function() {
-            var comp = module.exports=webpackJsonp([2],[],[328]);
+            var comp = module.exports=webpackJsonp([2],[],[324]);
             return { page: comp.default }
           })
         
