9b0d825549955b6258fe2fd4cfeb0b372e022f20
Jacob Andresen <jacob.andresen@gmail.com>
2017-05-20 18:31:29 +0200

    search in progress

--- components/searchautocomplete.js
+++ components/searchautocomplete.js
@@ -3,7 +3,7 @@ import Toolbar from 'react-md/lib/Toolbars';
 import Button from 'react-md/lib/Buttons';
 import FontIcon from 'react-md/lib/FontIcons';
 import Drawer from 'react-md/lib/Drawers';
-import AutocComplete from 'react-md/lib/Autocompletes/Autocomplete'
+import AutoComplete from 'react-md/lib/Autocompletes/Autocomplete'
 
 import {connect, dispatch} from 'react-redux'
 import { store, set}  from '../lib/store.js';
@@ -11,15 +11,15 @@ import { search } from '../lib/api.js';
 
 function SearchAutoComplete ({q,  isActive,  results}) {
   return (
-  <AutocComplete
+  <AutoComplete
     id="searchBooks"
     placeholder="Search"
     data={results}
     dataLabel="name"
     value={q}
     onChange={() => {
-      set(["search", "q"], q);
       search({q});
+      set(["search", "q"], this.props.value);
     }}
     block
     className="md-title--toolbar"
@@ -27,12 +27,11 @@ function SearchAutoComplete ({q,  isActive,  results}) {
     />);
 }
 
-
 function mapStateToProps(state) {
   let props = {
     isActive: state.getIn('search', 'isActive'),
     q: state.getIn(['search', 'q']),
-    results: state.getIn(['search', 'results'])
+    results: state.getIn(['search', 'results']).toJS()
   };
   console.log("props:", props);
   return props;
